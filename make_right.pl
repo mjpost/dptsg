a20663715ea448ac793388b3220ccb547e21f430
Matt Post <post@e14>
2009-09-22 22:26:17 -0400

    added script to right-binarize tree rules

---
+++ make_right.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl
+# 2009-09-22 Matt Post
+
+# Right-binarizes each node of a parse tree, naming intermediate nodes
+# xbar style.
+
+use strict;
+use warnings;
+use tree;
+
+while (my $line = <>) {
+  chomp($line);
+  my $subtree = build_subtree($line);
+
+  my $binarize = sub {
+    my ($node) = @_;
+
+    if ($node->{numkids} > 2) {
+      # create the new node (a hash) with the proper name
+      my $newnode = { 
+        label => xbar($node->{label}),
+        numkids => $node->{numkids} - 1
+      };
+      # replace the n-1 rightmost children of $node with $newnode,
+      # assigning those children to $newnode
+      @{$newnode->{children}}
+        = splice @{$node->{children}}, 1, $node->{numkids}-1, $newnode;
+    }
+  };
+
+  walk($subtree,[$binarize]);
+  print build_subtree_oneline($subtree), $/;
+}
+
+sub xbar {
+  my $label = shift;
+  return ($label =~ /'$/) ? $label : "$label'";
+}
