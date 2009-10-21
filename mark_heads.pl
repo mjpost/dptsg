d63f35f8d4bf180320fe2cf720181569cad747b4
Matt Post <post@e14>
2009-10-20 23:10:25 -0400

    scripts to explicitly mark the head of a parse tree structure, and then flatten a TSG to a PCFG while maintaining that explicitly-marked head information.  this is then used by the TSG parser (C++ version, adapted from Dan's impl of Collins model 1) to build statistics suitable for use in that parser.

---
+++ mark_heads.pl
@@ -0,0 +1,47 @@
+#!/usr/bin/perl
+
+# reads in a treebank and marks the head child of each node useful as
+# a preprocessing step when squashing a TSG down to a PCFG, if you
+# want to retain head information
+
+my $basedir;
+BEGIN {
+  $basedir = "$ENV{HOME}/code/dpinfer";
+  unshift @INC, $basedir;
+}
+
+use strict;
+use warnings;
+# use Devel::Peek qw/Dump/;
+use List::Util qw|reduce max min sum|;
+use TSG;
+
+my %PARAMS = (
+  lexicon => "$basedir/data/lex.02-21",
+);
+process_params(\%PARAMS,\@ARGV,\%ENV);
+
+my $head_symbol = '*';
+
+my $lexicon = read_lexicon($PARAMS{lexicon});
+
+while (my $line = <>) {
+  chomp($line);
+
+  my $subtree = build_subtree($line,$lexicon);
+  walk($subtree,[\&mark_head]);
+
+  print build_subtree_oneline($subtree,1), $/;
+}
+
+sub mark_head {
+  my ($node) = @_;
+
+  if ($node->{numkids} and ! is_preterminal($node)) {
+    my $hpos = $node->{hpos};
+    if ($hpos >= 0) {
+      my $head = @{$node->{children}}[$hpos];
+      $head->{label} .= $head_symbol;
+    }
+  }
+}
--- tsg2pcfg.pl
+++ tsg2pcfg.pl
@@ -19,38 +19,20 @@ use TSG;
 my %PARAMS = (
   lexicon => "$basedir/data/lex.02-21",
 );
-# check for environment variables overriding defaults
-foreach my $key (keys %PARAMS) {
-  if (exists $ENV{$key}) {
-    $PARAMS{$key} = $ENV{$key};
-    print STDERR "* $key = $PARAMS{$key} [env]\n";
-  }
-}
-# process command-line arguments
-while (@ARGV) {
-  my $arg = shift;
-
-  die "invalid option '$arg'" unless $arg =~ /^-/;
-
-  $arg =~ s/^-//g;
-
-  if (exists $PARAMS{$arg}) {
-    $PARAMS{$arg} = shift @ARGV;
-    print STDERR "* $arg = $PARAMS{$arg} [cmdline]\n";
-  } else {
-    die "no such option '$arg'";
-  }
-}
+process_params(\%PARAMS,\@ARGV,\%ENV);
 
 my $lexicon = read_lexicon($PARAMS{lexicon});
 
+my $SENTNO = 0;
 while (my $line = <>) {
   chomp($line);
 
+  $SENTNO++; 
+
   my $subtree = build_subtree($line,$lexicon);
   walk($subtree,[\&delete_nodes]);
-
   print build_subtree_oneline($subtree,1), $/;
+  walk($subtree,[\&verify_one_head]);
 }
 
 sub delete_nodes {
@@ -65,20 +47,75 @@ sub delete_nodes {
     $changed = 0;
     my $kidno = 0;
     my $numkids = scalar @{$node->{children}};
+
+    # see if any kid has its head explicitly marked
+    my $headpos = -1;
     for my $kidno (0..$numkids-1) {
       my $kid = @{$node->{children}}[$kidno];
-      if ($kid->{label} =~ /^\*/) {
+      if (has_marked_head($kid)) {
+        $headpos = $kidno;
+        last;
+      }
+    }
+
+    for my $kidno (0..$numkids-1) {
+      my $kid = @{$node->{children}}[$kidno];
+      if (is_internal($kid)) {
         $changed = 1;
+
+        # case 1: if the child we are collapsing is not explicitly
+        # marked as the head (and another child is), we need to remove
+        # the head markings from all the children we are bringing up
+        if ($headpos >= 0 and $headpos != $kidno) {
+          map { $_->{label} =~ s/\*$// } @{$kid->{children}};
+        }
+
         # if the internal node is a preterminal, rename it so that it
         # expands deterministically to its child; otherwise, delete it.
         if (is_preterminal($kid)) {
           my $grandkid = @{$kid->{children}}[0];
-          $kid->{label} = "[$grandkid->{label}]";
+          $kid->{label} = clean($kid->{label}) ."_". delex($grandkid->{label});
+          $kid->{label} .= '*' if ($headpos >= 0 and $headpos == $kidno);
         } else {
           splice @{$node->{children}}, $kidno, 1, @{$kid->{children}};
         }
+
+        last;
       }
       $kidno++;
     }
   } while $changed;
+}
+
+sub verify_one_head {
+  my ($node) = @_;
+
+  if (! is_preterminal($node) and @{$node->{children}}) {
+    my $headcount = 0;
+    my $numkids = @{$node->{children}};
+    for my $kidno (0..$numkids-1) {
+      my $kid = @{$node->{children}}[$kidno];
+      $headcount++ if ($kid->{label} =~ /\*$/);
+    }
+
+    die "* FATAL: sentence $SENTNO " . ruleof($node). " has $headcount heads\n"
+        if ($headcount != 1);
+  }
+}
+
+sub has_marked_head {
+  my ($node) = @_;
+  return ($node->{label} =~ /\*$/) ? 1 : 0;
+}
+
+sub is_internal {
+  my ($node) = @_;
+  return ($node->{label} =~ /^\*/) ? 1 : 0;
+}
+
+sub clean {
+  my ($label) = @_;
+  $label =~ s/^\*//;
+  $label =~ s/\*$//;
+  return $label;
 }
