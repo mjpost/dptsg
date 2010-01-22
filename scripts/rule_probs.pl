d723d34bc2bcff181abccb8065eb615847cd1c42
Matt Post <post@e14>
2010-01-21 22:05:08 -0500

    version that (kind of) does what's described in footnote 3 of Klein & Manning (2003)

--- scripts/rule_probs.pl
+++ scripts/rule_probs.pl
@@ -32,14 +32,14 @@ use List::Util qw|sum|;
 
 my %PARAMS = (
   '*counts' => 0,     # produce counts instead of probabilities
-  rulethresh => 0,
+  rulethresh => 1,    # rules with lower counts are thrown out
   lexicon => "$basedir/data/lex.02-21",
-  thresh => 0,     # min count of rule
+  thresh => 1,        # if extracting from a tree, min count for lex items
 );
 process_params(\%PARAMS,\@ARGV,\%ENV);
 my $lexicon = read_lexicon($PARAMS{lexicon},$PARAMS{thresh});
 
-my %COUNTS;
+my (%COUNTS,%UNKCOUNTS,%WORDCOUNTS,%TAGCOUNTS,%CLASSCOUNTS);
 
 while (my $line = <>) {
   chomp($line);
@@ -58,6 +58,8 @@ while (my $line = <>) {
   }
 }
 
+my $total_words = sum values %WORDCOUNTS;
+
 my %LHS_COUNTS;
 # sum lefthand sides
 foreach my $lhs (keys %COUNTS) {
@@ -73,14 +75,45 @@ foreach my $lhs (keys %COUNTS) {
   }
 }
 
+my $k = 0.5;
 foreach my $lhs (keys %COUNTS) {
   foreach my $rule (sort { $COUNTS{$lhs}{$b} <=> $COUNTS{$lhs}{$a} } keys %{$COUNTS{$lhs}}) {
     if ($PARAMS{counts}) {
       print "$COUNTS{$lhs}{$rule} $rule\n";
     } else {
-      my $pr = ($COUNTS{$lhs}{$rule} / $LHS_COUNTS{$lhs});
+      my $pr;
+      if ($rule =~ /^\(\S+ _(\S+)_\)$/) {
+        my $word = $1;
+
+        my $class = classof($word);
+        my $p_tag_word = ($COUNTS{$lhs}{$rule} + $k * tag_prob($lhs,classof($word))) / ($WORDCOUNTS{$word} + $k);
+        if ($lhs eq "IN") {
+          # print "* $COUNTS{$lhs}{$rule}\n";
+          # print "* $k\n";
+          # print "* tag_prob($lhs,$class) = " . tag_prob($lhs,classof($word)) . $/;
+          # print "* WORDCOUNTS($word) $WORDCOUNTS{$word}\n";
+        }
+ 
+        # P(word|tag) = P(tag|word) * P(word) / P(tag)
+        $pr = $p_tag_word * ($WORDCOUNTS{$word}/$total_words) / ($TAGCOUNTS{$lhs}/$total_words);
+        
+      } else {
+        $pr = ($COUNTS{$lhs}{$rule} / $LHS_COUNTS{$lhs});
+      }
       print "$pr $rule\n";
     }
+  }
+}
+
+foreach my $tag (keys %UNKCOUNTS) {
+  foreach my $class (keys %{$UNKCOUNTS{$tag}}) {
+    # my $tag_prob = tag_prob($tag,$class);
+    # my $pr = $tag_prob * ($TAGCOUNTS{$tag} / $total_words) / ($CLASSCOUNTS{$class} / $total_words);
+
+    my $pr = ($UNKCOUNTS{$tag}{$class} / $total_words) / ($TAGCOUNTS{$tag} / $total_words);
+    print "* $tag $class $UNKCOUNTS{$tag}{$class} $TAGCOUNTS{$tag} $total_words ($pr)\n";
+
+    print "$pr ($tag _${class}_)\n";
   }
 }
 
@@ -93,6 +126,24 @@ sub count_rule {
   if ($lhs !~ /^\*/ and @{$node->{children}}) {
     my $rule = ruleof($node);
     $COUNTS{$lhs}{$rule}++;
+
+    if (is_preterminal($node)) {
+      my $word = delex(@{$node->{children}}[0]->{label});
+      # print "UNKCOUNTS($lhs," . classof($word) . ")\n";
+      $UNKCOUNTS{$lhs}{classof($word)}++;
+      $WORDCOUNTS{$word}++;
+      $TAGCOUNTS{$lhs}++;
+      $CLASSCOUNTS{classof($word)}++;
+    }
   }
 }
 
+# computes Pr(class | tag)
+sub tag_prob {
+  my ($tag,$class) = @_;
+
+  my $pr = $UNKCOUNTS{$tag}{$class} / $TAGCOUNTS{$tag};
+  # print "TAG_PROB($tag,$class) = $pr\n";
+
+  return $pr;
+}
