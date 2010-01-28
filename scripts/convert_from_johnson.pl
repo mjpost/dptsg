8eab7fe4e81440aacf298bb23bb8ed50acbebc76
Matt Post <post@e14>
2010-01-27 21:57:13 -0500

    added script that unflattens a tree using the map built when the grammar was flattened

---
+++ scripts/convert_from_johnson.pl
@@ -0,0 +1,79 @@
+#!/usr/bin/perl
+
+my $basedir;
+BEGIN {
+  $basedir = "$ENV{HOME}/code/dpinfer";
+  unshift @INC, $basedir;
+}
+
+use strict;
+use warnings;
+use TSG;
+
+# load the map
+
+my %rule_mapping;
+
+my $map_file = shift @ARGV;
+open READ, $map_file or die "can't open file $map_file";
+while (my $line = <READ>) {
+  chomp($line);
+  my ($flat,$full) = split(/ \|\|\| /,$line);
+  
+  $rule_mapping{$flat} = $full;
+}
+close READ;
+
+my (%rules,%map);
+
+while (my $line = <>) {
+  chomp($line);
+
+  my $tree = build_subtree($line);
+
+  walk($tree,[\&unflatten]);
+
+  print build_subtree_oneline($tree), $/;
+}
+
+sub substitute_subtree {
+  # node is the current node in the subtree, and nodes is the list of
+  # nodes yest to be matched with the leaves of the subtree node is a
+  # part of
+  my ($node,$nodes,$isroot) = @_;
+  $isroot = 1 unless defined $isroot;
+
+  $node->{label} = "*" . $node->{label} unless $isroot;
+
+  my $numkids = @{$node->{children}};
+
+#   print "  SUB($node->{label},[", join(" ",map{$_->{label}}@$nodes), "]) $numkids kids\n";
+
+  for my $k (0..$#{$node->{children}}) {
+    my $kid = @{$node->{children}}[$k];
+    if ($kid->{numkids} == 0) {
+      my $repl = shift @$nodes;
+#         print "   SUBBING $kid->{label} = $repl->{label}\n";
+      @{$node->{children}}[$k] = $repl;
+    } else {
+      substitute_subtree($kid,$nodes,0);
+    }
+  }
+
+  return $node;
+}
+
+sub unflatten {
+  my ($node) = @_;
+
+  my $rule = ruleof($node,1);
+
+  if (exists $rule_mapping{$rule}) {
+    my $full_rule = $rule_mapping{$rule};
+
+    my $subtree = build_subtree($full_rule);
+    my @kids = @{$node->{children}};
+    substitute_subtree($subtree,\@kids);
+    $node->{children} = $subtree->{children};
+  }
+}
