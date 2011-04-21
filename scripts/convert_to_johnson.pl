b4436442ce4bdd842f89f6cd53eb5f753088475e
Matt Post <post@jhu.edu>
2011-04-21 12:00:28 -0400

    reverted terminal-focused binarization (slower!)

--- scripts/convert_to_johnson.pl
+++ scripts/convert_to_johnson.pl
@@ -37,32 +37,9 @@ while (my $line = <>) {
 }
 
 while (my ($rule,$prob) = each %rules) {
-  my @tokens = split(' --> ',$rule);
-  my $flatrule = "(" . join(" ",@tokens) . ")";
-  my $tree = build_subtree($flatrule);
-  binarize_subtree({ node=>$tree, dir=>"terminal", collapse=>"none", unique=>1 });
-
-  my @rules;
-  extract_subtrees($tree,\@rules);
-
-  print "$prob " . parens_to_arrow($rules[0]) . $/;
-  shift @rules;
-  foreach my $rule (@rules) {
-	print "1.0 " . parens_to_arrow($rule) . $/;
-  }
-
-  # print "$prob $rule\n";
+  print "$prob $rule\n";
 }
 
 while (my ($flat,$full) = each %map) {
   print STDERR "$flat ||| $full\n";
-}
-
-sub parens_to_arrow {
-  my ($parens) = @_;
-
-  $parens =~ s/\(|\)//g;
-  my @tokens = split(' ',$parens);
-
-  return $tokens[0] . " --> " . join(" ",@tokens[1..$#tokens]);
 }
